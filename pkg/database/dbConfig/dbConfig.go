13e8c2e10dd47b7a093683afc77e6ef180cda248
hudayberdipolat <hudayberdipolat@gmail.com>
2024-02-14 09:47:35 +0500

    ready database connection

---
+++ pkg/database/dbConfig/dbConfig.go
@@ -0,0 +1,40 @@
+package dbconfig
+
+import (
+	"fmt"
+
+	"github.com/hudayberdipolat/go-Ecommerce-backend/pkg/config"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+type DbConfig struct {
+	config *config.Config
+}
+
+func NewDBConnection(config *config.Config) *DbConfig {
+	return &DbConfig{
+		config: config,
+	}
+}
+
+func (dbConfig DbConfig) GetDBConfig() (*gorm.DB, error) {
+
+	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=%s",
+		dbConfig.config.DBConfig.DbHost,
+		dbConfig.config.DBConfig.DbUser,
+		dbConfig.config.DBConfig.DbPassword,
+		dbConfig.config.DBConfig.DbName,
+		dbConfig.config.DBConfig.DbPort,
+		dbConfig.config.DBConfig.DbSllMode,
+	)
+
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{
+		TranslateError: true,
+	})
+
+	if err != nil {
+		return nil, err
+	}
+	return db, err
+}
