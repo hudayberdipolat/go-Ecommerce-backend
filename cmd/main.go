7b356a5d82e199cfba5820f14cf48cac651ac05b
hudayberdipolat <hudayberdipolat@gmail.com>
2024-02-15 12:29:17 +0500

    ready run server

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,25 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"log"
 
+	"github.com/hudayberdipolat/go-Ecommerce-backend/internal/app"
+)
+
+func main() {
+	getDependencies, err := app.GetDependencies()
+	if err != nil {
+		log.Fatal("error : ", err.Error())
+	}
+	// constructor
+
+	// new app
+	runSever := fmt.Sprintf("%s:%s", getDependencies.Config.HttpConfig.ServerHost, getDependencies.Config.HttpConfig.ServerPort)
+
+	appRouter := app.NewApp(getDependencies)
+
+	if errRunServer := appRouter.Listen(runSever); errRunServer != nil {
+		log.Println("error : ", errRunServer.Error())
+	}
 }
