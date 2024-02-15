4acb8f1283468dc7c0edecda8f680b71b6c77722
hudayberdipolat <hudayberdipolat@gmail.com>
2024-02-15 12:21:40 +0500

    ready NewApp http

--- internal/app/app.go
+++ internal/app/app.go
@@ -1,1 +1,28 @@
 package app
+
+import (
+	"time"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+func NewApp(dependencies *Dependencies) (httpServer *fiber.App) {
+	httpServer = fiber.New(fiber.Config{
+		ServerHeader: dependencies.Config.HttpConfig.AppHeader,
+		AppName:      dependencies.Config.HttpConfig.AppName,
+		BodyLimit:    30 * 1024 * 1024,
+		ReadTimeout:  30 * time.Second,
+		WriteTimeout: 30 * time.Second,
+		ErrorHandler: func(ctx *fiber.Ctx, err error) error {
+			code := fiber.StatusInternalServerError
+			if e, ok := err.(*fiber.Error); ok {
+				code = e.Code
+			}
+			return ctx.Status(code).JSON(fiber.Map{
+				"status":  code,
+				"message": "Näsazlyk ýüze çykdy, Sonrak synanysyn!!!",
+			})
+		},
+	})
+	return httpServer
+}
