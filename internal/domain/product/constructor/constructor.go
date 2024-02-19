ae7f1b37b711669caaf085717448fadd0bfe310b
hudayberdipolat <hudayberdipolat@gmail.com>
2024-02-19 15:06:09 +0500

    ready product dto request and response

--- internal/domain/product/constructor/constructor.go
+++ internal/domain/product/constructor/constructor.go
@@ -1,1 +1,10 @@
 package constructor
+
+import (
+	"github.com/hudayberdipolat/go-Ecommerce-backend/pkg/config"
+	"gorm.io/gorm"
+)
+
+func ProductRequirementsCreator(db *gorm.DB, config config.Config) {
+
+}
--- internal/domain/product/dto/productResponse.go
+++ internal/domain/product/dto/productResponse.go
@@ -1,1 +1,69 @@
 package dto
+
+import "github.com/hudayberdipolat/go-Ecommerce-backend/internal/models"
+
+type OneProductResponse struct {
+	ID            int    `json:"id"`
+	ProductNameTk string `json:"product_name_tk"`
+	ProductNameRu string `json:"product_name_ru"`
+	ProductSlug   string `json:"product_slug"`
+	ProductDescTk string `json:"product_desc_tk"`
+	ProductDescRu string `json:"product_desc_ru"`
+	ProductStatus string `json:"product_status"`
+	MainImage     string `json:"Main_image"`
+	Price         string `json:"price"`
+	TotalCount    int    `json:"total_count"`
+	GalanSany     int    `json:"galan_sany"`
+}
+
+func NewOneProductResponse(product models.Product) OneProductResponse {
+	return OneProductResponse{
+		ID:            product.ID,
+		ProductNameTk: product.ProductNameTk,
+		ProductNameRu: product.ProductNameRu,
+		ProductSlug:   product.ProductSlug,
+		ProductDescTk: product.ProductDescTk,
+		ProductDescRu: product.ProductDescRu,
+		ProductStatus: product.ProductStatus,
+		MainImage:     product.MainImage,
+		Price:         product.Price,
+		TotalCount:    product.TotalCount,
+		GalanSany:     product.GalanSany,
+	}
+}
+
+type AllProductResponse struct {
+	ID            int    `json:"id"`
+	ProductNameTk string `json:"product_name_tk"`
+	ProductNameRu string `json:"product_name_ru"`
+	ProductSlug   string `json:"product_slug"`
+	ProductDescTk string `json:"product_desc_tk"`
+	ProductDescRu string `json:"product_desc_ru"`
+	ProductStatus string `json:"product_status"`
+	MainImage     string `json:"Main_image"`
+	Price         string `json:"price"`
+	TotalCount    int    `json:"total_count"`
+	GalanSany     int    `json:"galan_sany"`
+}
+
+func NewAllProductResponse(products []models.Product) []AllProductResponse {
+	var allProductResponse []AllProductResponse
+
+	for _, product := range products {
+		productResponse := AllProductResponse{
+			ID:            product.ID,
+			ProductNameTk: product.ProductNameTk,
+			ProductNameRu: product.ProductNameRu,
+			ProductSlug:   product.ProductSlug,
+			ProductDescTk: product.ProductDescTk,
+			ProductDescRu: product.ProductDescRu,
+			ProductStatus: product.ProductStatus,
+			MainImage:     product.MainImage,
+			Price:         product.Price,
+			TotalCount:    product.TotalCount,
+			GalanSany:     product.GalanSany,
+		}
+		allProductResponse = append(allProductResponse, productResponse)
+	}
+	return allProductResponse
+}
