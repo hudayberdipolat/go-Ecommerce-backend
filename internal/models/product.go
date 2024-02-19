0b850c9516dd1662fd46937b8e935bfa7999f0fd
hudayberdipolat <hudayberdipolat@gmail.com>
2024-02-19 14:53:24 +0500

    update product model

--- internal/models/product.go
+++ internal/models/product.go
@@ -2,14 +2,16 @@ package models
 
 type Product struct {
 	ID            int    `json:"id"`
-	ProductName   string `json:"product_name"`
+	ProductNameTk string `json:"product_name_tk"`
+	ProductNameRu string `json:"product_name_ru"`
 	ProductSlug   string `json:"product_slug"`
-	ProductDesc   string `json:"product_desc"`
-	ImageUrl      string `json:"image_url"`
+	ProductDescTk string `json:"product_desc_tk"`
+	ProductDescRu string `json:"product_desc_ru"`
+	ProductStatus string `json:"product_status"`
+	MainImage     string `json:"Main_image"`
 	Price         string `json:"price"`
 	TotalCount    int    `json:"total_count"`
 	GalanSany     int    `json:"galan_sany"`
-	ProductStatus string `json:"product_status"`
 }
 
 func (*Product) TableName() string {
