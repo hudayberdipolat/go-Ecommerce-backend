b543b33f56b8c7fa1910a32633ed9038bf4e091c
hudayberdipolat <hudayberdipolat@gmail.com>
2024-02-19 15:23:59 +0500

    ready product repository

--- internal/domain/product/repository/productRepository.go
+++ internal/domain/product/repository/productRepository.go
@@ -1,1 +1,11 @@
 package repository
+
+import "github.com/hudayberdipolat/go-Ecommerce-backend/internal/models"
+
+type ProductRepository interface {
+	FindOne(productID int) (*models.Product, error)
+	FindAll() ([]models.Product, error)
+	Create(product models.Product) error
+	Update(productID int, product models.Product) error
+	Delete(productID int) error
+}
--- internal/domain/product/repository/productRepositoryImp.go
+++ internal/domain/product/repository/productRepositoryImp.go
@@ -1,1 +1,54 @@
 package repository
+
+import (
+	"github.com/hudayberdipolat/go-Ecommerce-backend/internal/models"
+	"gorm.io/gorm"
+)
+
+type productRepositoryImp struct {
+	db *gorm.DB
+}
+
+func NewProductRepository(db *gorm.DB) ProductRepository {
+	return productRepositoryImp{
+		db: db,
+	}
+}
+
+func (p productRepositoryImp) FindOne(productID int) (*models.Product, error) {
+	var product models.Product
+	if err := p.db.First(&product, productID).Error; err != nil {
+		return nil, err
+	}
+	return &product, nil
+}
+
+func (p productRepositoryImp) FindAll() ([]models.Product, error) {
+	var products []models.Product
+
+	if err := p.db.Find(&products).Error; err != nil {
+		return nil, err
+	}
+	return products, nil
+}
+
+func (p productRepositoryImp) Create(product models.Product) error {
+	if err := p.db.Create(&product).Error; err != nil {
+		return err
+	}
+	return nil
+}
+
+func (p productRepositoryImp) Update(productID int, product models.Product) error {
+	if err := p.db.Model(&models.Product{}).Where("id=?", productID).Updates(&product).Error; err != nil {
+		return err
+	}
+	return nil
+}
+
+func (p productRepositoryImp) Delete(productID int) error {
+	if err := p.db.Unscoped().Delete(models.Product{}, productID).Error; err != nil {
+		return err
+	}
+	return nil
+}
