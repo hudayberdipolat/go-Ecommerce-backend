85d9569b9db6b4342bb1d52a78ed7d436a187138
hudayberdipolat <hudayberdipolat@gmail.com>
2024-02-20 14:23:26 +0500

    add phoneNumberValidate

--- internal/utils/validate/validate.go
+++ internal/utils/validate/validate.go
@@ -1,9 +1,24 @@
 package validate
 
-import "github.com/go-playground/validator/v10"
+import (
+	"regexp"
+
+	"github.com/go-playground/validator/v10"
+)
 
 func ValidateStruct(data interface{}) error {
 	validate := validator.New()
 	err := validate.Struct(data)
 	return err
+}
+
+func PhoneNumberValidate(phoneNumber string) bool {
+	if string(phoneNumber[3]) == "6" {
+		r := regexp.MustCompile("[9936]{4}[1-5][0-9]{6}$")
+		return r.MatchString(phoneNumber)
+	}
+	if string(phoneNumber[3]) == "7" {
+		return string(phoneNumber[4]) == "1"
+	}
+	return false
 }
