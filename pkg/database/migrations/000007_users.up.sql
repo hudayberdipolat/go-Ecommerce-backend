0f6aab96c4e37a7cedd0c9b26c94480d4214171e
hudayberdipolat <hudayberdipolat@gmail.com>
2024-02-21 09:45:52 +0500

    user table add unique key for phone_number column

--- pkg/database/migrations/000007_users.up.sql
+++ pkg/database/migrations/000007_users.up.sql
@@ -3,7 +3,7 @@ CREATE TABLE IF NOT EXISTS users(
     id SERIAL PRIMARY KEY,
     firstname VARCHAR(50) NOT NULL,
     surname VARCHAR(50) NOT NULL,
-    phone_number VARCHAR(50) NOT NULL,
+    phone_number VARCHAR(50) UNIQUE  NOT NULL,
     password VARCHAR(255) NOT NULL,
     created_at TIMESTAMP NOT NULL,
     updated_at TIMESTAMP NOT NULL
