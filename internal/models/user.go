6028cbdc9cc27d5dcb09ebab681e550f034e8c40
hudayberdipolat <hudayberdipolat@gmail.com>
2024-02-21 09:38:42 +0500

    create user table and model

--- internal/models/user.go
+++ internal/models/user.go
@@ -4,10 +4,12 @@ import "time"
 
 type User struct {
 	ID          int       `json:"id"`
-	FullName    string    `json:"full_name"`
+	Firstname   string    `json:"firstname"`
+	Surname     string    `json:"surname"`
 	PhoneNumber string    `json:"phone_number"`
 	Password    string    `json:"password"`
 	CreatedAt   time.Time `json:"created_at"`
+	UpdatedAt   time.Time `json:"updated_at"`
 }
 
 func (*User) TableName() string {
---
+++ pkg/database/migrations/000007_users.up.sql
@@ -0,0 +1,10 @@
+CREATE TYPE userStatus as ENUM ('active', 'passive');
+CREATE TABLE IF NOT EXISTS users(
+    id SERIAL PRIMARY KEY,
+    firstname VARCHAR(50) NOT NULL,
+    surname VARCHAR(50) NOT NULL,
+    phone_number VARCHAR(50) NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    created_at TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP NOT NULL
+);
