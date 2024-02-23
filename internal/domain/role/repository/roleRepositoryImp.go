395ffc57ad89bd693c54f930f8e360ee3737be72
hudayberdipolat <hudayberdipolat@gmail.com>
2024-02-23 10:48:19 +0500

    role repository readt

--- internal/domain/role/repository/roleRepositoryImp.go
+++ internal/domain/role/repository/roleRepositoryImp.go
@@ -1,6 +1,8 @@
 package repository
 
 import (
+	"errors"
+
 	"github.com/hudayberdipolat/go-Ecommerce-backend/internal/models"
 	"gorm.io/gorm"
 )
@@ -16,21 +18,45 @@ func NewRoleRepository(db *gorm.DB) RoleRepository {
 }
 
 func (roleRepo roleRepositoryImp) FindOne(roleID int) (*models.Role, error) {
-	panic("role repo imp")
+	var role models.Role
+	if err := roleRepo.db.First(&role, roleID).Error; err != nil {
+		return nil, err
+	}
+	return &role, nil
 }
 
 func (roleRepo roleRepositoryImp) FindAll() ([]models.Role, error) {
-	panic("role repo imp")
+	var roles []models.Role
+	if err := roleRepo.db.Find(&roles).Error; err != nil {
+		return nil, err
+	}
+	return roles, nil
 }
 
 func (roleRepo roleRepositoryImp) Create(role models.Role) error {
-	panic("role repo imp")
+	if err := roleRepo.db.Create(&role).Error; err != nil {
+		if errors.Is(err, gorm.ErrDuplicatedKey) {
+			return errors.New("Bu role ady eýýäm bar!!!")
+		}
+		return err
+	}
+	return nil
 }
 
 func (roleRepo roleRepositoryImp) Update(roleID int, role models.Role) error {
-	panic("role repo imp")
+	if err := roleRepo.db.Model(&models.Role{}).Where("id=?", roleID).Updates(&role).Error; err != nil {
+		if errors.Is(err, gorm.ErrDuplicatedKey) {
+			return errors.New("Bu role ady eýýäm bar!!!")
+		}
+		return err
+	}
+	return nil
 }
 
 func (roleRepo roleRepositoryImp) Delete(roleID int) error {
-	panic("role repo imp")
+	var role models.Role
+	if err := roleRepo.db.Unscoped().Delete(&role, roleID).Error; err != nil {
+		return err
+	}
+	return nil
 }
