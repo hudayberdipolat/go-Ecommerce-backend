8d21457c529678e485bee5cbedf5312cdca2c142
hudayberdipolat <hudayberdipolat@gmail.com>
2024-02-23 14:14:45 +0500

    ready role handler

--- internal/domain/role/handler/roleHandlerImp.go
+++ internal/domain/role/handler/roleHandlerImp.go
@@ -1,8 +1,14 @@
 package handler
 
 import (
+	"net/http"
+	"strconv"
+
 	"github.com/gofiber/fiber/v2"
+	"github.com/hudayberdipolat/go-Ecommerce-backend/internal/domain/role/dto"
 	"github.com/hudayberdipolat/go-Ecommerce-backend/internal/domain/role/service"
+	"github.com/hudayberdipolat/go-Ecommerce-backend/internal/utils/response"
+	"github.com/hudayberdipolat/go-Ecommerce-backend/internal/utils/validate"
 )
 
 type roleHandlerImp struct {
@@ -16,21 +22,77 @@ func NewRoleHandler(service service.RoleService) RoleHandler {
 }
 
 func (roleHandler roleHandlerImp) GetOne(ctx *fiber.Ctx) error {
-	panic("role handler imp")
+	roleID, _ := strconv.Atoi(ctx.Params("roleID"))
+	role, err := roleHandler.roleService.GetOneRole(roleID)
+	if err != nil {
+		errResponse := response.Error(http.StatusNotFound, "role not found", err.Error(), nil)
+		return ctx.Status(http.StatusNotFound).JSON(errResponse)
+	}
+	successResponse := response.Success(http.StatusOK, "get role data", role)
+	return ctx.Status(http.StatusOK).JSON(successResponse)
 }
 
 func (roleHandler roleHandlerImp) GetAll(ctx *fiber.Ctx) error {
-	panic("role handler imp")
+	role, err := roleHandler.roleService.GetAllRole()
+	if err != nil {
+		errResponse := response.Error(http.StatusNotFound, "roles not found", err.Error(), nil)
+		return ctx.Status(http.StatusNotFound).JSON(errResponse)
+	}
+	successResponse := response.Success(http.StatusOK, "get all roles data", role)
+	return ctx.Status(http.StatusOK).JSON(successResponse)
 }
 
 func (roleHandler roleHandlerImp) Create(ctx *fiber.Ctx) error {
-	panic("role handler imp")
+	var roleRequet dto.RoleRequest
+
+	if err := ctx.BodyParser(&roleRequet); err != nil {
+		errResponse := response.Error(http.StatusBadRequest, "body parser error", err.Error(), nil)
+		return ctx.Status(http.StatusBadRequest).JSON(errResponse)
+	}
+
+	if err := validate.ValidateStruct(roleRequet); err != nil {
+		errResponse := response.Error(http.StatusBadRequest, "body parser error", err.Error(), nil)
+		return ctx.Status(http.StatusBadRequest).JSON(errResponse)
+	}
+	// create role
+
+	if err := roleHandler.roleService.CreateRole(roleRequet); err != nil {
+		errResponse := response.Error(http.StatusInternalServerError, "role created error", err.Error(), nil)
+		return ctx.Status(http.StatusInternalServerError).JSON(errResponse)
+	}
+	successResponse := response.Success(http.StatusOK, "role created successfully", nil)
+	return ctx.Status(http.StatusOK).JSON(successResponse)
 }
 
 func (roleHandler roleHandlerImp) Update(ctx *fiber.Ctx) error {
-	panic("role handler imp")
+	var roleRequet dto.RoleRequest
+	roleID, _ := strconv.Atoi(ctx.Params("roleID"))
+
+	if err := ctx.BodyParser(&roleRequet); err != nil {
+		errResponse := response.Error(http.StatusBadRequest, "body parser error", err.Error(), nil)
+		return ctx.Status(http.StatusBadRequest).JSON(errResponse)
+	}
+
+	if err := validate.ValidateStruct(roleRequet); err != nil {
+		errResponse := response.Error(http.StatusBadRequest, "body parser error", err.Error(), nil)
+		return ctx.Status(http.StatusBadRequest).JSON(errResponse)
+	}
+
+	// update role
+	if err := roleHandler.roleService.UpdateRole(roleID, roleRequet); err != nil {
+		errResponse := response.Error(http.StatusInternalServerError, "role updated error", err.Error(), nil)
+		return ctx.Status(http.StatusInternalServerError).JSON(errResponse)
+	}
+	successResponse := response.Success(http.StatusOK, "role updated successfully", nil)
+	return ctx.Status(http.StatusOK).JSON(successResponse)
 }
 
 func (roleHandler roleHandlerImp) Delete(ctx *fiber.Ctx) error {
-	panic("role handler imp")
+	roleID, _ := strconv.Atoi(ctx.Params("roleID"))
+	if err := roleHandler.roleService.DeleteRole(roleID); err != nil {
+		errResponse := response.Error(http.StatusInternalServerError, "role deleted error", err.Error(), nil)
+		return ctx.Status(http.StatusInternalServerError).JSON(errResponse)
+	}
+	successResponse := response.Success(http.StatusOK, "role deleted successfully", nil)
+	return ctx.Status(http.StatusOK).JSON(successResponse)
 }
