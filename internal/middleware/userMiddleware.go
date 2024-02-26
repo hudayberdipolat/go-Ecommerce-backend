442d862863c39d549eec7e3dbc2bccb2b2695b73
hudayberdipolat <hudayberdipolat@gmail.com>
2024-02-26 09:34:01 +0500

    user middleware ready

---
+++ internal/middleware/userMiddleware.go
@@ -0,0 +1,41 @@
+package middleware
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/golang-jwt/jwt"
+	"github.com/hudayberdipolat/go-Ecommerce-backend/internal/utils/response"
+	"github.com/hudayberdipolat/go-Ecommerce-backend/pkg/jwtToken/userToken"
+)
+
+func UserMiddleware(ctx *fiber.Ctx) error {
+	token := ctx.Get("Authorization")
+	if token == "" {
+		errResponse := response.Error(http.StatusUnauthorized, "Unauthorized", "Unauthorized", nil)
+		return ctx.Status(fiber.StatusUnauthorized).JSON(errResponse)
+	}
+	claims, err := verifyUserToken(token)
+	if err != nil {
+		errResponse := response.Error(http.StatusUnauthorized, "Invalid token", "Invalid token", nil)
+		return ctx.Status(fiber.StatusUnauthorized).JSON(errResponse)
+	}
+	ctx.Locals("phone_number", claims.PhoneNumber)
+	ctx.Locals("user_id", claims.UserID)
+	return ctx.Next()
+}
+
+func verifyUserToken(tokenString string) (*userToken.Claims, error) {
+	//userToken.SecretKey
+	token, err := jwt.ParseWithClaims(tokenString, &userToken.Claims{}, func(token *jwt.Token) (interface{}, error) {
+		return userToken.SecretKey, nil
+	})
+	if err != nil {
+		return nil, err
+	}
+	if claims, ok := token.Claims.(*userToken.Claims); ok && token.Valid {
+		return claims, nil
+	}
+	return nil, fmt.Errorf("Invalid token")
+}
